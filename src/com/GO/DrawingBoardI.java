342f415107bd0ed9fe31c816647780e80c90f58d
Kinga Krata <kobietazkosmosu@gmail.com>
2016-11-21 13:16:23 +0100

    Inititial commit

---
+++ src/com/GO/DrawingBoardI.java
@@ -0,0 +1,9 @@
+/**
+ * Bartłomiej Woś 2016-11-21.
+ */
+
+package com.GO;
+public interface DrawingBoardI {
+
+
+}
---
+++ src/com/GO/PLAYER.java
@@ -0,0 +1,7 @@
+/**
+ * Kinga Krata 2016-11-20.
+ */
+
+package com.GO;
+
+public enum PLAYER {BLACK,WHITE};

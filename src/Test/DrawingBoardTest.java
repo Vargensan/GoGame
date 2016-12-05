3a8b206b2268f12fe9e060458499c1da7034d117
Kinga Krata <kobietazkosmosu@gmail.com>
2016-12-05 12:31:18 +0100

    added gitignore to Server module

--- src/Test/DrawingBoardTest.java
+++ src/Test/DrawingBoardTest.java
@@ -15,6 +15,7 @@ public class DrawingBoardTest extends DrawingBoard{
     ClientGUI app = new ClientGUI();
     PLACE[][] GameBoard = new PLACE[19][19];
 
+
     @Test
     public void testDrawing(){
         app.startDrawing(GameBoard);
--- src/com/GO/Play.java
+++ src/com/GO/Play.java
@@ -1,5 +1,7 @@
 package com.GO;
 
+import GUIGo.ClientGUI;
+
 /**
  * Kinga Krata 2016-12-05.
  */
@@ -7,11 +9,18 @@ public class Play {
 
     private Board playBoard;
     private Client clientSocket;
+    private ClientGUI window;
     Play()
     {
          playBoard=new Board(19);
+
          clientSocket=new Client();
          clientSocket.listenSocket();
+
+         window=new ClientGUI();
+
+         window.startDrawing(playBoard.getGameTable());
+
 
          clientSocket.out.println("aaa");
 

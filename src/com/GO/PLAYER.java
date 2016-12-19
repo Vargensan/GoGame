310fc62c7bb0368f9fd3faf22155eb4b59668404
Bartłomiej <wosbartek@gmail.com>
2016-12-19 14:22:17 +0100

    Simple Mistake

--- src/com/GO/PLAYER.java
+++ src/com/GO/PLAYER.java
@@ -8,6 +8,14 @@ public enum PLAYER {
 
     BLACK, WHITE;
 
+    public PLAYER getEnemyColor(PLAYER color){
+        if(color == PLAYER.BLACK)
+            return PLAYER.WHITE;
+        else
+            return PLAYER.BLACK;
+
+    }
+
     public PLACE playerToPlace() {
         switch (this) {
             case BLACK:

8519af9460942ca171ac74f63530cc14db9f01b5
Kinga Krata <kobietazkosmosu@gmail.com>
2016-12-23 23:53:54 +0100

    server exits properly, adding deads works one time

--- Server/src/ClientThread.java
+++ Server/src/ClientThread.java
@@ -1,57 +1,89 @@
 import java.net.Socket;
 
-class ClientThread extends Thread{
+class ClientThread extends Thread
+{
 
-    boolean bussy = false;
-    boolean activegame;
-    int numberofthread_active;
+    private boolean busy = false;
+    private boolean activeGame;
+    private int numberofthread_active;
     private Socket clientSocket = null;
     private final ClientThread[] threads;
-    private int maxClientsCount;
-    GamePlay game;
+    private int maxClientCount;
+    private GamePlay game;
+
     /*
     Add implementation to 1-1 connection between clients
     Dunno, on same port or what
      */
-    public ClientThread(Socket clientSocket, ClientThread[] threads) {
+
+    public ClientThread(Socket clientSocket, ClientThread[] threads)
+    {
         this.clientSocket = clientSocket;
         this.threads = threads;
-        maxClientsCount = threads.length;
+        maxClientCount = threads.length;
     }
-    public void run(){
-        int maxClientsCount = this.maxClientsCount;
+
+
+    public void run()
+    {
+        int maxClientCount = this.maxClientCount;
         ClientThread[] threads = this.threads;
+        
             /*Opis
             Dla każdego wątku sprawdź czy posiada nie pustą grę
             jeżeli taką posiada, sprawdź czy jest dostępna
             jeżeli jest dostępna to sprawdź liczbę graczy, jeżeli
             liczba graczy to jeden, to dodaj wątek do tej gry;
              */
-        synchronized (this) {
-            for (int i = 0; i < maxClientsCount; i++) {
-                if(threads[i]!=null){
-                    //if (!threads[i].bussy) {
-                    if (threads[i].game != null) {
-                        if (threads[i].game.getGameStatus() == false) {
-                            if (threads[i].game.getNumberofPlayer() == false) {
-                                activegame = true;
+
+        synchronized (this)
+        {
+            for (int i = 0; i < maxClientCount; i++)
+            {
+                if (threads[i] != null)
+                {
+                    //if (!threads[i].busy) {
+                    if (threads[i].game != null)
+                    {
+                        if (threads[i].game.getGameStatus() == false)
+                        {
+                            if (threads[i].game.getNumberOfPlayers() == false)
+                            {
+                                activeGame = true;
                                 numberofthread_active = i;
-                               // System.out.println("Found active game on thread: "+i);
                             }
                         }
                     }
                 }
             }
 
-            if (activegame) {
-              //  System.out.println("I am coming to game of thread: "+numberofthread_active);
-                threads[numberofthread_active].game.setClient(clientSocket);
-            } else {
-                //System.out.println("I am hosting game...");
-                game = new GamePlay(threads);
-                bussy = true;
-                game.setClient(clientSocket);
+            if (activeGame)
+            {
+
+                //-----------TRY-CATCH FOR SUPPRESSING EXCEPTION PRINT-----------//
+                try
+                {
+
+                    //  System.out.println("I am coming to game of thread: "+numberofthread_active);
+                    threads[numberofthread_active].game.setClient(clientSocket);
+                } catch (Exception e) {
+                    threads[numberofthread_active].game.killGame();
+                }
+
+            } else
+            {
+                //-----------TRY-CATCH FOR SUPPRESSING EXCEPTION PRINT-----------//
+                try
+                {
+                    game = new GamePlay(threads);
+                    busy = true;
+                    game.setClient(clientSocket);
+                } catch (Exception e)
+                {
+                    game.killGame();
+                }
             }
-        } //synchronized
-    } // run
-}
+        }
+    } //synchronized
+} // run
+
